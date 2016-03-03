d9e029c36e9da7ed91f5722433871be582509c58
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2016-03-03 09:52:37 -0500

    Project created

---
+++ ChatApplication/src/main/java/ChatApplication/ChatApplication/App.java
@@ -0,0 +1,13 @@
+package ChatApplication.ChatApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
