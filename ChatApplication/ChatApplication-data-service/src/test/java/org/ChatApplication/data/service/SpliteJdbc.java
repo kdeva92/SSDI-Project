8a2a3a5b806c150b7f6fed1da8c6df1e1d189942
komal-ingale <kingale@uncc.edu>
2016-03-04 02:11:17 -0500

    Added Maven module and dependecies

---
+++ ChatApplication/ChatApplication-data-service/src/test/java/org/ChatApplication/data/service/SpliteJdbc.java
@@ -0,0 +1,41 @@
+package org.ChatApplication.data.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import junit.framework.TestCase;
+
+public class SpliteJdbc extends TestCase {
+
+	public SpliteJdbc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void testApp() {
+
+
+		Connection c = null;
+		Statement stmt = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+			System.out.println("Opened database successfully");
+
+			stmt = c.createStatement();
+			String sql = "CREATE TABLE COMPANY " + "(ID INT PRIMARY KEY     NOT NULL,"
+					+ " NAME           TEXT    NOT NULL, " + " AGE            INT     NOT NULL, "
+					+ " ADDRESS        CHAR(50), " + " SALARY         REAL)";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.close();
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Table created successfully");
+
+	
+	
+	}
+}
