c7ac0593ed2b337a11b2b9e64c9d0dc7f6351db4
komal-ingale <kingale@uncc.edu>
2016-03-26 13:43:00 -0400

    DAO and Service layer changes for getUser nad searchUser

--- ChatApplication/ChatApplication-data-service/src/main/java/org/ChatApplication/data/DAO/UserDAO.java
+++ ChatApplication/ChatApplication-data-service/src/main/java/org/ChatApplication/data/DAO/UserDAO.java
@@ -1,9 +1,12 @@
 package org.ChatApplication.data.DAO;
 
+import java.util.List;
+
 import org.ChatApplication.data.entity.User;
 import org.ChatApplication.data.util.HibernateUtil;
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 
 /**
@@ -31,6 +34,35 @@ public class UserDAO {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		sessionFactory.getCurrentSession().save(user);
 		logger.info("Leaving createUser");
+	}
+
+	public User getUser(String email, String password) throws HibernateException, Exception {
+		logger.info("Entering getUser");
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Query query = sessionFactory.getCurrentSession()
+				.createQuery("from User where email = :email and password =: password");
+		query.setParameter("email", email);
+		query.setParameter("password", password);
+		List<User> users = query.list();
+		if (users == null || users.size() <= 0) {
+			return null;
+		}
+		logger.info("Leaving getUser");
+		return users.get(0);
+
+	}
+
+	public List<User> getUsers(String searchString) throws HibernateException, Exception {
+		logger.info("Entering getUser");
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Query query = sessionFactory.getCurrentSession()
+				.createQuery("from User where email = :email or ninerId =: ninerId or firstName =:firstname");
+		query.setParameter("email", searchString);
+		query.setParameter("ninerId", searchString);
+		query.setParameter("firstName", searchString);
+		List<User> users = query.list();
+		logger.info("Leaving getUser");
+		return users;
 	}
 
 }
--- ChatApplication/ChatApplication-data-service/src/main/java/org/ChatApplication/data/service/UserService.java
+++ ChatApplication/ChatApplication-data-service/src/main/java/org/ChatApplication/data/service/UserService.java
@@ -1,5 +1,7 @@
 package org.ChatApplication.data.service;
 
+import java.util.List;
+
 import org.ChatApplication.data.DAO.DAOObjectFactory;
 import org.ChatApplication.data.entity.User;
 import org.ChatApplication.data.util.HibernateSessionUtil;
@@ -40,4 +42,29 @@ public class UserService {
 			throw new Exception(e.getMessage());
 		}
 	}
+
+	public User getUser(String email, String password) throws Exception {
+		logger.info("Entering createUser");
+		User user = null;
+		try {
+			user = DAOObjectFactory.getUserDAO().getUser(email, password);
+		} catch (HibernateException e) {
+			logger.error(e.getMessage());
+			throw new Exception(e.getMessage());
+		}
+		return user;
+	}
+
+	public List<User> getUsers(String searchString) throws Exception {
+		logger.info("Entering createUser");
+		List<User> users = null;
+		try {
+			users = DAOObjectFactory.getUserDAO().getUsers(searchString);
+		} catch (HibernateException e) {
+			logger.error(e.getMessage());
+			throw new Exception(e.getMessage());
+		}
+		return users;
+	}
+
 }
