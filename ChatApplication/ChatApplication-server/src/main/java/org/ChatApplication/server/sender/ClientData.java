607305b84fe969198f6c2435ee26a985459de26a
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2016-03-27 15:48:17 -0400

    Added message unpacking and flow till message handler

---
+++ ChatApplication/ChatApplication-server/src/main/java/org/ChatApplication/server/sender/ClientData.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.ChatApplication.server.sender;
+
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+
+/**
+ * @author Devdatta
+ *
+ */
+public class ClientData {
+	SocketChannel socketChannel;
+	SelectionKey selectionKey;
+	
+	ClientData(SocketChannel socketChannel, SelectionKey selectionKey){
+		this.selectionKey=selectionKey;
+		this.socketChannel = socketChannel;
+	}
+
+	public SocketChannel getSocketChannel() {
+		return socketChannel;
+	}
+
+	public void setSocketChannel(SocketChannel socketChannel) {
+		this.socketChannel = socketChannel;
+	}
+
+	public SelectionKey getSelectionKey() {
+		return selectionKey;
+	}
+
+	public void setSelectionKey(SelectionKey selectionKey) {
+		this.selectionKey = selectionKey;
+	}
+}
