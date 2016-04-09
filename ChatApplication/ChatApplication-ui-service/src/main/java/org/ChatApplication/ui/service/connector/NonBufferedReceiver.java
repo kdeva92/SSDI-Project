997022736a6157bfc5c3ce4c4380a69f5aebc371
komal-ingale <kingale@uncc.edu>
2016-04-09 12:36:51 -0400

    Receiver to handle empty buffer

--- ChatApplication/ChatApplication-ui-service/src/main/java/org/ChatApplication/ui/service/connector/NonBufferedReceiver.java
+++ ChatApplication/ChatApplication-ui-service/src/main/java/org/ChatApplication/ui/service/connector/NonBufferedReceiver.java
@@ -6,6 +6,7 @@ package org.ChatApplication.ui.service.connector;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketException;
 import java.nio.ByteBuffer;
 
 import org.ChatApplication.common.converter.ByteToEntityConverter;
@@ -26,22 +27,21 @@ public class NonBufferedReceiver {
 
 	InputStreamReader streamreader;
 	public MessageListener messageListener;
-	public NonBufferedReceiver(Socket socket,MessageListener ml) throws IOException {
+
+	public NonBufferedReceiver(Socket socket, MessageListener ml) throws IOException {
 		// TODO Auto-generated constructor stub
 		this.messageListener = ml;
 		streamreader = new InputStreamReader(socket.getInputStream());
 		new Thread(new ReceiverPoller()).start();
 	}
-	
-	class ReceiverPoller implements Runnable{
+
+	class ReceiverPoller implements Runnable {
 
 		public void run() {
-			// TODO Auto-generated method stub
 			byte[] data = new byte[Message.MAX_MESSAGE_SIZE];
 			int size = 0;
 			int read;
-			while(true){
-				
+			while (true) {
 				try {
 					read = streamreader.read();
 				} catch (IOException e) {
@@ -49,59 +49,59 @@ public class NonBufferedReceiver {
 					e.printStackTrace();
 					continue;
 				}
-				if(read == Message.START_OF_MESSAGE){
+				if (read == -1) {
+					// TODO: handle gracefully
+					break;
+				}
+
+				if (read == Message.START_OF_MESSAGE) {
 					data = new byte[Message.MAX_MESSAGE_SIZE];
-				}
-				else if(read == Message.END_OF_MESSAGE){
+				} else if (read == Message.END_OF_MESSAGE) {
 					Message message = MessageUtility.getMessage(ByteBuffer.wrap(data));
-					//uncomment below for test
-					//NonBufferedReceiverTest.showReceivedMessage(message);	
-					
-					
-							try {
-								User recd_user = ByteToEntityConverter.getInstance().getUser(message.getData());
-								messageListener.updateLogin(recd_user);
-							} catch (JsonParseException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-								try {
-									messageListener.updateLogin(null);
-								} catch (IOException e1) {
-									// TODO Auto-generated catch block
-									e1.printStackTrace();
-								}								
-							} catch (JsonMappingException e) {
-								// TODO Auto-generated catch block
-								try {
-									messageListener.updateLogin(null);
-								} catch (IOException e1) {
-									// TODO Auto-generated catch block
-									e1.printStackTrace();
-								}								
-								e.printStackTrace();
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								try {
-									messageListener.updateLogin(null);
-								} catch (IOException e1) {
-									// TODO Auto-generated catch block
-									e1.printStackTrace();
-								}								
-								e.printStackTrace();
-							}
+					// uncomment below for test
+					// NonBufferedReceiverTest.showReceivedMessage(message);
+					System.out.println(new String(message.getData()));
+
+					try {
+						User recd_user = ByteToEntityConverter.getInstance().getUser(message.getData());
 						
-					
+						messageListener.updateLogin(recd_user);
+					} catch (JsonParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						try {
+							messageListener.updateLogin(null);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+					} catch (JsonMappingException e) {
+						// TODO Auto-generated catch block
+						try {
+							messageListener.updateLogin(null);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						try {
+							messageListener.updateLogin(null);
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						e.printStackTrace();
+					}
+
+				} else {
+					data[size++] = (byte) read;
 				}
-				else{
-					data[size++] = (byte)read; 
-				}
-				
-				
+
 			}
 		}
-		
+
 	}
-	
-	
-	
+
 }
